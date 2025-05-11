aaad9fb653f679cf67b900952af3ad2a83ba8dbc
thespekky <bandras123456@gmail.com>
2025-05-11 17:38:21 +0200

    már csak az időt kell megcsinálni

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const Szin=["red","blue","green"];
 class cube{
     x;
     y;
@@ -9,14 +11,10 @@ class cube{
         this.type=type;
         this.size=size;
     }
-     RandomBlock(x,y,type,size){
-        this.x=x;
-        this.y=y;
-        this.type=type;
-        this.size=size;
+    static random(){
+       return Szin[Math.floor(Math.random()*Szin.length)];
     }
 }
-
 
 var start_button=document.getElementById("start");
 var ranglista_button=document.getElementById("ranglista");
@@ -37,9 +35,14 @@ sound.style.display="none";
 reset_button.style.display="none";
 canvas.style.display="none";
 
-var pontok=3200;
+var pontok=0;
 var cubes=[];
-var cubesize=20;
+var cubesize=50;
+cubes.push(new cube(0,0,cube.random(),cubesize));
+
+var row_count;
+var column_count;
+var can_click=true;
 
 
  background_music.volume=music_slider.value/100;
@@ -52,12 +55,196 @@ document.addEventListener("click",function(){
     click_music.play();
 });
 
+function gamelost(){
+    alert("A játéknak vége");
+   reset_function();
+   var ranglista=[];
+   ranglista=JSON.parse(sessionStorage.getItem("ranglista"));
+    if(ranglista==null)
+    {
+        ranglista=[];
+    }
+   if(player_name===undefined||player_name=="")
+    {
+        player_name="Anonymus";
+    }
+   var new_player={
+       name:player_name,
+       score:pontok
+   }
+   ranglista.push(new_player);
+   sessionStorage.setItem("ranglista",JSON.stringify(ranglista));
+}
+async function raise_cubes_height(){
+    if(cubes.length==0)return;
+    for(let i=0;i<cubes.length;i++){
+        cubes[i].y-=cubesize;
+        if(cubes[i].y<=0){
+           gamelost();
+        }
+    }
+
+}
+async function fillrows(rows){
+    if(rows==0)return;
+   await raise_cubes_height();
+    var xpos=0;
+    var ypos=canvas.height-cubesize;
+    for(let i=0;i<row_count;i++){
+        var new_cube=new cube(xpos,ypos,cube.random(),cubesize);
+        xpos+=cubesize;
+        cubes.push(new_cube);
+    }
+    fillrows(rows-1);
+    /*for(let i=0;i<rows;i++){
+        cubes.push(new cube(Math.floor(Math.random()*(canvas.width-cubesize)),Math.floor(Math.random()*(canvas.height-cubesize)),cube.random(),cubesize));
+    }*/
+}
+
+function find_neighbours(clicked_cube,neighbours){
+    var found=false;
+    var same_cubes=[];
+    for (let i=0;i<cubes.length;i++)
+    {
+        var cube=cubes[i];
+        if (cube.x+cubesize==clicked_cube.x && cube.y==clicked_cube.y && cube.type==clicked_cube.type && neighbours.includes(cube)==false)
+        {
+            found=true;
+            same_cubes.push(cube);
+            neighbours.push(cube);
+        }
+        else if (cube.x-cubesize==clicked_cube.x && cube.y==clicked_cube.y && cube.type==clicked_cube.type && neighbours.includes(cube)==false)
+        {
+            found=true;
+            same_cubes.push(cube);
+            neighbours.push(cube);
+        }
+        else if (cube.x==clicked_cube.x && cube.y+cubesize==clicked_cube.y && cube.type==clicked_cube.type && neighbours.includes(cube)==false)
+        {
+            found=true;
+            same_cubes.push(cube);
+            neighbours.push(cube);
+        }
+        else if (cube.x==clicked_cube.x && cube.y-cubesize==clicked_cube.y && cube.type==clicked_cube.type && neighbours.includes(cube)==false)
+        {
+            found=true;
+            same_cubes.push(cube);
+            neighbours.push(cube);
+        }
+    }
+    if (found)
+    {
+        same_cubes.forEach(cube => {
+            find_neighbours(cube,neighbours);
+        });
+    }
+}
+function find_cube(c)
+{
+    var found=false;
+   var found_cube=cubes.find(cube=>{
+       return cube.x==c.x && cube.y==c.y+cubesize;
+   })
+   if(found_cube===undefined)
+   {
+        found=true;
+   }
+    return found;
+}
+
+function get_floating_cubes()
+{
+    var floating_cubes=[];
+    for (let i=0;i<cubes.length;i++)
+    {
+        var cube=cubes[i];
+        if (cube.y+cubesize<=canvas.height-cubesize)
+        {
+            if(find_cube(cube)&&floating_cubes.includes(cube)==false)
+            {
+                floating_cubes.push(cube);
+            }
+            
+        }
+    }
+    return floating_cubes;
+}
+
+async function fall_cubes(){
+    var floating_cubes=get_floating_cubes();
+    if(floating_cubes.length==0)
+    {
+        can_click=true;
+        fillrows(1);
+         return;
+    }
+    setTimeout(() => {
+        
+    }, 50);
+    floating_cubes.forEach(cube => {
+        cube.y+=cubesize;
+    });
+    setTimeout(fall_cubes,50);
+}
+function reset_function(){
+    start_button.style.display="block";
+    reset_button.style.display="none";
+    player_name_input.style.display="block";
+    ranglista_table.innerHTML="";
+    ranglista_button.style.display="block";
+    canvas.style.display="none";
+    music_slider.style.display="none";
+    sound.style.display="none";
+    settings_button.style.display="block";
+    cubes=[];
+}
+canvas.addEventListener("click",async function(event){
+    if(!can_click)return;
+    can_click=false;
+    var x=event.offsetX;
+    var y=event.offsetY;
+    var clicked_cube=cubes.find(cube=>{
+        return (cube.x<=x && cube.x+cubesize>=x) && (cube.y<=y && cube.y+cubesize>=y);
+    });
+    if(clicked_cube==undefined)
+    {
+        can_click=true; 
+        return;
+    }
+
+    var neighbours=[];
+    find_neighbours(clicked_cube,neighbours);
+    if (neighbours.length<3)
+    {
+        can_click=true;
+        return;
+    }
+    cubes=cubes.filter(cube=>{ 
+    if (neighbours.includes(cube))
+    {
+        pontok++;
+        return false;
+    }
+    else
+    { 
+        return true;
+    }
+    });
+    await fall_cubes();
+
+})
 
 music_slider.addEventListener("input",function(){
     background_music.volume=music_slider.value/100;
 });
 
-start_button.addEventListener("click",function(){
+start_button.addEventListener("click",async function(){
+    cubes=[];
+    canvas.width=Math.floor((window.innerWidth-30)/cubesize)*cubesize;
+    canvas.height=Math.floor((window.innerHeight-100)/cubesize)*cubesize;
+    row_count=Math.floor(canvas.width/cubesize);
+    column_count=Math.floor(canvas.height/cubesize);
+    await fillrows(3);
     player_name=document.getElementById("name").value;
     player_name_input.style.display="none";
     ranglista_button.style.display="none";
@@ -68,8 +255,8 @@ start_button.addEventListener("click",function(){
     settings_button.style.display="none";
     sound.style.display="none";
     music_slider.style.display="none";
-    canvas.width=window.innerWidth-30;
-    canvas.height=window.innerHeight-100;
+
+    
     
     ctx.fillStyle="white";
     ctx.fillRect(0,0,canvas.width,canvas.height);
@@ -84,18 +271,16 @@ ranglista_button.addEventListener("click",function(){
     settings_button.style.display="none";
     sound.style.display="none";
     music_slider.style.display="none";
-    ranglista_table.innerHTML=`<tr><th>nevek</th><th>pontok</th></tr> <tr><td>${player_name}</td><td>${pontok}</td></tr>`;
+    var ranglista=JSON.parse(sessionStorage.getItem("ranglista")); 
+    ranglista_table.innerHTML=`<thead> <tr><th>nevek</th><th>pontok</th></tr></thead> <tbody>`;
+    ranglista.forEach(player => {
+        ranglista_table.innerHTML+=`<tr><td>${player.name}</td><td>${player.score}</td></tr>`;
+    });
+    ranglista_table.innerHTML+=`</tbody>`;
 })
 reset_button.addEventListener("click",function(){
-    start_button.style.display="block";
-    reset_button.style.display="none";
-    player_name_input.style.display="block";
-    ranglista_table.innerHTML="";
-    ranglista_button.style.display="block";
-    canvas.style.display="none";
-    music_slider.style.display="none";
-    sound.style.display="none";
-    settings_button.style.display="block";
+    reset_function();
+
 })
 settings_button.addEventListener("click",function(){
     sound.style.display="block";
@@ -106,9 +291,8 @@ settings_button.addEventListener("click",function(){
 
 function animate()
 {
-    requestAnimationFrame(draw);
-    
-    setInterval(animate,20);
+    draw()
+    requestAnimationFrame(animate);
 }
 function draw()
 {
@@ -116,9 +300,14 @@ function draw()
     ctx.fillStyle="white";
     ctx.fillRect(0,0,canvas.width,canvas.height);
     cubes.forEach(cube => {
+       /*if(cube.y>0)
+        {
+            cube.y-=0.1;
+        }*/
         ctx.strokeStyle="black";
         ctx.fillStyle=cube.type;
         ctx.fillRect(cube.x,cube.y,cube.size,cube.size);
+        ctx.strokeRect(cube.x,cube.y,cube.size,cube.size);
     });
     ctx.restore();
 }
